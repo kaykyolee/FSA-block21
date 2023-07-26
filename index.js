9584649783ac66b6bcc754b18ec9e8e4cfd47e68
kaykyolee <kleegoma@gmail.com>
2023-07-26 14:57:20 -0400

    updating block21 modules and prototype chain assignment

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ class Car {
     this.year=year;
 }
 getDescription(){
-    return 'This car is a made from ${this.make} with ${this.model} model and it is made in ${this.year}. '
+    return `This car is a made from ${this.make} with ${this.model} model and it is made in ${this.year}.`
 }
 }
 
 class ElectricCar extends Car {
     constructor (make,model,year,range){
-    super(make,model,year)
+    super(make,model,year);
     this.range=range;
     }
 getDescription(){
-    return '${super.getDescription}. It has ${this.range} range.'
+    return `${super.getDescription()} It has ${this.range} range.`
 }
 }
 
-const electricCar1= new ElectricCar('Tesla','Model S','2019','300')
-console.log(electricCar1)
+const electricCar1= new ElectricCar('Tesla','Model S','2019','300');
+// console.log(electricCar1)
+console.log(electricCar1.getDescription())
